3914dd2e783b1ce1cf38de50f9046f61a65e41b1
yang19870713 <93074858@qq.com>
2016-06-23 22:49:23 +0800

    update searchbox

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,18 +1,38 @@
 import style from './search-box.scss';
 import classnames from "utils/classnames";
 
+const TREND = "trend";
+const SKILL = "skill";
+
 export default class SearchBox extends React.Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            category: props.category || TREND
+        }
+    }
+    toggleCategory(category){
+        if(this.state.category === category) {
+            return;
+        }
+        else {
+            this.setState({"category": category});
+        }
+    }
+    render() {
         return (
             <div>
                 <div className={style.btnGrp}>
-                    <div className={classnames(style.btn, style.active)}>
+                    <div className={classnames(style.btn, this.state.category===TREND?style.active:"")}
+                         onClick={()=>this.toggleCategory(TREND)}>
                         <i className="fa fa-line-chart"></i>
                     </div>
-                    <div className={style.btn}>
+                    <div className={classnames(style.btn, this.state.category===SKILL?style.active:"")}
+                        onClick={()=>this.toggleCategory(SKILL)}>
                         <i className="fa fa-stack-exchange"></i>
                     </div>
                 </div>
+
                 <form className={style.searchBox}
                     action="" name="trend" role="form">
                     <input id="keyword" type="text"
@@ -26,9 +46,8 @@ export default class SearchBox extends React.Component {
                             placeholder="Period"/>
                     <input className={style.submit}
                         type="submit" value="Search"/>
-                    {/*<input className={style.submit}
-                        type="submit" value="Search"/>*/}
                 </form>
+
                 <div className={style.slogon}>
                     <p>
                         A place
